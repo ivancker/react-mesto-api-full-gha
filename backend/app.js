21d453036d3fd8e791287a66e1eed60b46bdaa35
Ivan Kovalenko <gunonu@yandex.ru>
2023-08-09 19:30:00 +0300

    15 1.7

--- backend/app.js
+++ backend/app.js
@@ -33,6 +33,6 @@ app.use(errorLogger);
 app.use(errors());
 app.use(handleError);
 
-app.listen(8080, () => {
-  console.log('Server is running on port 8080');
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });

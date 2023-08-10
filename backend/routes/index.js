d0c42687cad101bbea4686b8ca353cdc157fab8d
Ivan Kovalenko <gunonu@yandex.ru>
2023-08-10 13:46:54 +0300

    15 3.0

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,7 +4,7 @@ const cardRouter = require('./cards');
 const usersController = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const { validateSignUp, validateSignIn } = require('../middlewares/validate');
-const NotFoundError = require('../errors/notFoundError'); // 404
+// const NotFoundError = require('../errors/notFoundError'); // 404
 
 router.post('/signup', validateSignUp, usersController.createUser);
 router.post('/signin', validateSignIn, usersController.loginUser);
@@ -12,8 +12,8 @@ router.post('/signin', validateSignIn, usersController.loginUser);
 router.use('/users', auth, userRouter);
 router.use('/cards', auth, cardRouter);
 
-router.use(auth, () => {
-  throw new NotFoundError('Такая страница не существует');
-});
+// router.use(auth, () => {
+//   throw new NotFoundError('Такая страница не существует');
+// });
 
 module.exports = router;

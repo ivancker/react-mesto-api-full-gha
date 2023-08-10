555c107cc981c7ba44fc9257791ecd8f57526b0e
Ivan Kovalenko <gunonu@yandex.ru>
2023-08-10 11:57:30 +0300

    15 2.6

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const bodyParser = require('body-parser'); // без этого пост не работает
 const helmet = require('helmet');
-const corsConfig = require('./middlewares/cors');
+// const corsConfig = require('./middlewares/cors');
 const handleError = require('./middlewares/handleError');
 const routers = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -13,7 +13,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 const app = express();
-app.use(cors(corsConfig));
+app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json()); // подключение body-parser к app
 app.use(bodyParser.urlencoded({ extended: true }));

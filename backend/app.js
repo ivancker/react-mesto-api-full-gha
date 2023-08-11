025ede7e0f77ceddf27e7b18c1f5d0bb7637b945
Ivan Kovalenko <gunonu@yandex.ru>
2023-08-11 19:41:44 +0300

    15 5.1

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 const app = express();
 app.use(cors());
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: ['https://mestoo.nomoreparties.sbs'] }));
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-credentials', 'true');
 //   res.header('Acces-Control-Allow-Origin', '*');
